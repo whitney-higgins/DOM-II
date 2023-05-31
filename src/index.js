42931074b5810314a024ad4b3de8f9d7db47cb0d
whitney-higgins <whitneyxhiggins@gmail.com>
2023-05-30 20:44:47 -0500

    three events added to page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import './less/index.less'
+import "./less/index.less";
 
-// Your code goes here!
+const homeBtn = document.querySelector(".nav .nav-link");
+homeBtn.addEventListener("mouseover", (event) => {
+  event.target.style.color = "red";
+});
+
+const keyPressed = document.querySelector("h2");
+
+function changeColor(event) {
+  if (event.key === "Control") {
+    keyPressed.style.fontWeight = "bold";
+  }
+}
+
+document.addEventListener("keydown", changeColor);
+
+// const mainImg = document.querySelector(".intro img");
+// console.log(mainImg);
+
+// mainImg.addEventListener("focus", (event) => {
+//   event.
+// });
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons[0].addEventListener("dblclick", (event) => {
+  event.target.textContent = "Ouch!";
+});
